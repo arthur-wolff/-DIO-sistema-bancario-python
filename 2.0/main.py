c87fd4b9a3d92bfd98514dbba03380708e523fe6
Arthur <arthurwolffsoares@gmail.com>
2024-07-08 16:38:03 -0300

    2.0

--- 2.0/main.py
+++ 2.0/main.py
@@ -1,10 +1,14 @@
+import textwrap
+
+
 def menu ():
     menu    =   """
     [1] Depositar
     [2] Sacar
     [3] Extrato
-    [4] Nova conta
-    [5] Novo usuario
+    [4] Novo usuario
+    [5] Nova conta
+    [6] Contas
     [0] Sair
     Insira a operacao: """
     return input(menu)
@@ -14,6 +18,7 @@ def depositar (saldo, deposito, extrato, /):
             saldo += deposito
             print   (f"Valor depositado = R${deposito:.2f}\n")
             extrato += f"Deposito: R${deposito:.2f}\n"
+            return saldo, extrato
     else:
         print   ("Valor invalido")
 
@@ -24,6 +29,7 @@ def sacar (*, saldo, saque, extrato, limite, n_saques, limite_saques):
         saldo_insuficiente = saque > saldo
         limite_insuficiente = saque > limite
         n_saques_insuficientes = n_saques >= limite_saques
+        
         if saldo_insuficiente:
             print("Saldo insuficiente")
         elif limite_insuficiente:
@@ -34,27 +40,63 @@ def sacar (*, saldo, saque, extrato, limite, n_saques, limite_saques):
             saldo -= saque
             extrato += f"Saque: R$ {saque:.2f}\n"
             n_saques += 1
+            
+            
         else:
             print("Operacao invalida")
+        
+        return saldo, extrato
 
-def extrato (saldo,/, *, extrato):
-    extrato =  """ 
-    EXTRATO
+def imprimir_extrato (saldo,/, *, extrato):
+    return print(extrato + f"Saldo: R${saldo:.2f}")
 
+def criar_usuario (usuarios):
+    cpf = input("Insira o CPF do usuario: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+    
+    if usuario:
+        print  ("Usuario já existente\n")
+        return
+    else:
+        print   ("_________Novo cadastro_________\n")
+        nome = input("Insira o nome do Usuario: ")
+        data_nasc = input("Insira a data de nascimento: ")
+        endereco = input("Insira o endereço (RUA, Numero - BAIRRO - CIDADE/ESTADO): ")
+        usuarios.append({"nome": nome, "data de nascimento": data_nasc,"cpf": cpf, "endereco": endereco})
+        print ("Usuario Cadastrado\n")
+                
+def filtrar_usuario (cpf, usuarios):
+    usuario_filtrado = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuario_filtrado[0] if usuario_filtrado else None
+
+def criar_conta (agencia, n_conta, usuarios):
+    cpf = input("Insira o cpf a ser consultado: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+    
+    if usuario:
+        print   ("\nConta Criada")
+        return {"agencia": agencia, "n_conta": n_conta, "usuario": usuario}
+    else:
+        print("\nUsuario não encontrado\n")
+
+def listar_contas (contas):
+    for conta in contas:
+        linha = f"""\ 
+        Agencia :\t {conta['agencia']}
+        C/C:\t\t{conta['n_conta']}
+        Titular:\t{conta['usuario']['nome']}
     """
-    return print(extrato)
-
-def criar_usuario (usario):
-    return
-def filtrar_usuario (cpf, usuario):
-    return
-
-
+    print ("=" * 100)
+    print (textwrap.dedent(linha))
+    
+     
 def main ():
-    LIMITE_SAQUES = 0
+    LIMITE_SAQUES = 3
+    ANGENCIA = "0001"
+    
     saldo = 0
     limite = 500
-    extrato = ""
+    extrato = """_________EXTRATO_________\n"""
     n_saques = 0
     usuarios = []
     contas = []
@@ -65,18 +107,28 @@ def main ():
         if controle == "1":
             deposito = float(input("Insira o Valor a ser depositado: "))
             
-            depositar(saldo, deposito, extrato)
+            saldo, extrato = depositar(saldo, deposito, extrato)
         elif controle == "2":
             saque = float(input("Insira o Valor a ser saquado: "))
             
-            sacar(saldo = saldo, saque = saque,limite = limite, n_saques = n_saques,
-                  limite_saques = LIMITE_SAQUES )
+            saldo, extrato = sacar(saldo = saldo, saque = saque, extrato = extrato ,limite = limite, 
+                                   n_saques = n_saques,limite_saques = LIMITE_SAQUES )
         elif controle == "3":
-            print(extrato)
+            imprimir_extrato(saldo, extrato = extrato)
         elif controle == "4":
             criar_usuario(usuarios)
-        
+            
+        elif controle == "5":
+            n_conta = len(contas)+1
+            conta = criar_conta(ANGENCIA, n_conta, usuarios)
+            if conta:
+                contas.append(conta)
+        elif controle == "6":
+            listar_contas(contas)
+            
         elif controle == "0":
             break
+        else:
+            print("\n_________OPERACAO INVALIDA_________\n")
 
 main()
